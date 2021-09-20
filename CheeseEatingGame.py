509d7a86b81a4429cf5b7564f6b76ed7f269bcac
edenjudeurias <edenjudeurias@gmail.com>
2021-09-20 13:54:08 -0500

    added files

---
+++ CheeseEatingGame.py
@@ -0,0 +1,8 @@
+import random
+import time
+
+weight = input('How much do you weigh? \n')
+
+print('You weigh ' + str(weight) + ' lbs.')
+
+print('This value will change as the game goes on.')
---
+++ run.sh
@@ -0,0 +1,6 @@
+echo This program will clear your current terminal tab. To continue press enter.
+read varc
+sudo apt install python
+sudo apt install python-pip
+rmdir Game\ Files
+mkdir 'Game Files' && cd Game\ Files
