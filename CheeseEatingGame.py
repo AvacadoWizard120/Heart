86210a8e906d6f7359ce80949b9d6e640f951b09
edenjudeurias <edenjudeurias@gmail.com>
2021-09-22 09:58:01 -0500

    More changes

--- CheeseEatingGame.py
+++ CheeseEatingGame.py
@@ -3,6 +3,16 @@ import time
 
 weight = input('How much do you weigh? \n')
 
-print('You weigh ' + str(weight) + ' lbs.')
+print('\n You weigh ' + str(weight) + ' lbs.')
 
-print('This value will change as the game goes on.')
+print('\n This value will change as the game goes on.')
+
+print('\n During this game you will make descisions. /n These descisions will affect your wieght, in turn, affecting your health.')
+
+print('\n The more wieght you gain the higher your risk of a Heart Attack')
+
+print('\n If your risk goes above a certain threshhold, you will have a heart attack. By default there is a 6 in 10 chance you will die')
+
+q1 = str('This is a test question.')
+
+
--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 echo This program will clear your current terminal tab. To continue press enter.
 read varc
+clear
 sudo apt install python
 sudo apt install python-pip
+sudo apt install python-tk
+pip install pygame
 rmdir Game\ Files
-mkdir 'Game Files' && cd Game\ Files
+mkdir 'Game Files'
+clear
+python CheeseEatingGame.py
